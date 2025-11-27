9c4acbba7c32f0d1fab81ece04846c0c28bc4d9a
Raúl Verdejo <verdejomartinraul@gmail.com>
2025-11-27 16:09:58 +0100

    27/11/2025 at 16:09

--- unidad5/peliculas/index.js
+++ unidad5/peliculas/index.js
@@ -1,176 +1,189 @@
-const datos = {
-  "Search": [
-    {
-      "Title": "Star Wars: Episode IV - A New Hope",
-      "Year": "1977",
-      "imdbID": "tt0076759",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BOGUwMDk0Y2MtNjBlNi00NmRiLTk2MWYtMGMyMDlhYmI4ZDBjXkEyXkFqcGc@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Star Wars: Episode V - The Empire Strikes Back",
-      "Year": "1980",
-      "imdbID": "tt0080684",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMTkxNGFlNDktZmJkNC00MDdhLTg0MTEtZjZiYWI3MGE5NWIwXkEyXkFqcGc@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Star Wars: Episode VI - Return of the Jedi",
-      "Year": "1983",
-      "imdbID": "tt0086190",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNWEwOTI0MmUtMGNmNy00ODViLTlkZDQtZTg1YmQ3MDgyNTUzXkEyXkFqcGc@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Star Wars: Episode VII - The Force Awakens",
-      "Year": "2015",
-      "imdbID": "tt2488496",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Star Wars: Episode I - The Phantom Menace",
-      "Year": "1999",
-      "imdbID": "tt0120915",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BODVhNGIxOGItYWNlMi00YTA0LWI3NTctZmQxZGUwZDEyZWI4XkEyXkFqcGc@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Star Wars: Episode III - Revenge of the Sith",
-      "Year": "2005",
-      "imdbID": "tt0121766",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Star Wars: Episode II - Attack of the Clones",
-      "Year": "2002",
-      "imdbID": "tt0121765",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNTgxMjY2YzUtZmVmNC00YjAwLWJlODMtNDBhNzllNzIzMjgxXkEyXkFqcGc@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Rogue One: A Star Wars Story",
-      "Year": "2016",
-      "imdbID": "tt3748528",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Star Wars: Episode VIII - The Last Jedi",
-      "Year": "2017",
-      "imdbID": "tt2527336",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Star Trek",
-      "Year": "2009",
-      "imdbID": "tt0796366",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMjE5NDQ5OTE4Ml5BMl5BanBnXkFtZTcwOTE3NDIzMw@@._V1_SX300.jpg"
-    }
-  ],
-  "totalResults": "4947",
-  "Response": "True"
-}
+var nombre = "";         
+var cont = 1;             
+var anio = "";             
+var contenedor;
+var input;                
+var inputAnio;
+var cargando = false;   
+var divFlotanteDetalle = null;  
+var detenerScroll = false;     
 
-const datos2 = {
-   "Search":[
-      {
-         "Title":"Star Wars: Episode IX - The Rise of Skywalker",
-         "Year":"2019",
-         "imdbID":"tt2527338",
-         "Type":"movie",
-         "Poster":"https://m.media-amazon.com/images/M/MV5BODg5ZTNmMTUtYThlNy00NjljLWE0MGUtYmQ1NDg4NWU5MjQ1XkEyXkFqcGc@._V1_SX300.jpg"
-      },
-      {
-         "Title":"Star Trek Into Darkness",
-         "Year":"2013",
-         "imdbID":"tt1408101",
-         "Type":"movie",
-         "Poster":"https://m.media-amazon.com/images/M/MV5BMTk2NzczOTgxNF5BMl5BanBnXkFtZTcwODQ5ODczOQ@@._V1_SX300.jpg"
-      },
-      {
-         "Title":"A Star Is Born",
-         "Year":"2018",
-         "imdbID":"tt1517451",
-         "Type":"movie",
-         "Poster":"https://m.media-amazon.com/images/M/MV5BNGUxZTc0NTAtNzQwMy00MmM2LTgzMGYtZWIyY2E1MGFjYmM5XkEyXkFqcGc@._V1_SX300.jpg"
-      },
-      {
-         "Title":"Solo: A Star Wars Story",
-         "Year":"2018",
-         "imdbID":"tt3778644",
-         "Type":"movie",
-         "Poster":"https://m.media-amazon.com/images/M/MV5BOTM2NTI3NTc3Nl5BMl5BanBnXkFtZTgwNzM1OTQyNTM@._V1_SX300.jpg"
-      },
-      {
-         "Title":"Star Trek Beyond",
-         "Year":"2016",
-         "imdbID":"tt2660888",
-         "Type":"movie",
-         "Poster":"https://m.media-amazon.com/images/M/MV5BN2FhNDI1YTYtNWI0OC00ZjYxLWJlYWEtN2ZiZjRmZDY1MWJjXkEyXkFqcGc@._V1_SX300.jpg"
-      },
-      {
-         "Title":"Star Trek: The Next Generation",
-         "Year":"1987–1994",
-         "imdbID":"tt0092455",
-         "Type":"series",
-         "Poster":"https://m.media-amazon.com/images/M/MV5BMmNiNTQ2YzYtODBjYy00ZWMwLTlmNWMtYWE1NTQ2ZTYyZmMwXkEyXkFqcGc@._V1_SX300.jpg"
-      },
-      {
-         "Title":"Star Trek: Discovery",
-         "Year":"2017–2024",
-         "imdbID":"tt5171438",
-         "Type":"series",
-         "Poster":"https://m.media-amazon.com/images/M/MV5BOTZkZTc1ZmYtYTA3Mi00MzZhLWFmZTItZTM2NThjYWJlZGU4XkEyXkFqcGc@._V1_SX300.jpg"
-      },
-      {
-         "Title":"Star Wars: The Clone Wars",
-         "Year":"2008–2020",
-         "imdbID":"tt0458290",
-         "Type":"series",
-         "Poster":"https://m.media-amazon.com/images/M/MV5BOTdiODQ1MDYtZjM5My00MmQ5LTk1ZWUtZWIwYjBhZGMxZDAyXkEyXkFqcGc@._V1_SX300.jpg"
-      },
-      {
-         "Title":"Star Trek: First Contact",
-         "Year":"1996",
-         "imdbID":"tt0117731",
-         "Type":"movie",
-         "Poster":"https://m.media-amazon.com/images/M/MV5BZjc0OWE1NDMtNjRmMi00ZDc2LWI2ZmQtNzEwNWU4Nzg4NDA5XkEyXkFqcGc@._V1_SX300.jpg"
-      },
-      {
-         "Title":"Star Trek II: The Wrath of Khan",
-         "Year":"1982",
-         "imdbID":"tt0084726",
-         "Type":"movie",
-         "Poster":"https://m.media-amazon.com/images/M/MV5BOTFkZDY5NWQtMjY4Yy00OThiLTk3YjQtMDdhYTllMTI4MTdiXkEyXkFqcGc@._V1_SX300.jpg"
-      }
-   ],
-   "totalResults":"4947",
-   "Response":"True"
-}
-
-window.onload = ()=>{
-    let contenedor = document.getElementById("general");
-    document.getElementById("btnCargar").addEventListener("click", ()=>{
-        maquetar(contenedor, datos2);
-    })
-    maquetar(contenedor, datos);
-}
-
-function maquetar(contenedor, array){
-    for(peli of array.Search){
+function maquetar(data){
+    for(const peli of data.Search){
         let divPeli = document.createElement("div");
         let titulo = document.createElement("p");
         let imagen = document.createElement("img");
-
+        let id = peli.imdbID;
+        divPeli.id = id;
         titulo.innerHTML = peli.Title;
         imagen.src = peli.Poster;
-
         divPeli.appendChild(imagen);
         divPeli.appendChild(titulo);
         contenedor.appendChild(divPeli);
+
+        //Evento para coger la id
+        divPeli.addEventListener("click", (e)=>{
+            if(e.target){
+                peticionDetalle(peli.imdbID);
+            }
+        })
     }
+}
+
+function peticionDetalle(id){
+    contenedor.classList.add("opaco");
+    document.body.classList.add("estatico");
+    let url = "http://www.omdbapi.com/?i=" + id + "&apikey=c0f0795e&plot=full"
+    fetch(url).then(response => response.json())
+    .then(data =>{
+        maquetacionClick(data);
+    })
+}
+
+function maquetacionClick(data){
+    if(divFlotanteDetalle){
+        divFlotanteDetalle.remove();//para  que solo haya 1 flotante
+    }
+
+    detenerScroll = true;
+
+    divFlotanteDetalle = document.createElement("div");
+    divFlotanteDetalle.classList.add("flotante");
+
+    let divMaquetado = document.createElement("div");
+    divMaquetado.classList.add("detalle-pelicula");
+    
+    let tituloDetalle = document.createElement("h2");
+    tituloDetalle.innerHTML = data.Title;
+    
+    let posterDetalle = document.createElement("img");
+    posterDetalle.src = data.Poster === "N/A" ? "placeholder.png" : data.Poster;
+    posterDetalle.alt = "Poster de " + data.Title;
+    
+    let directorDetalle = document.createElement("p");
+    directorDetalle.innerHTML = "<strong>Director:</strong> " + data.Director;
+    
+    let actoresDetalle = document.createElement("p");
+    actoresDetalle.innerHTML = "<strong>Actores:</strong> " + data.Actors;
+    
+    let sinopsisDetalle = document.createElement("p");
+    sinopsisDetalle.innerHTML = "<strong>Sinopsis:</strong> " + data.Plot;
+    
+    let anioDetalle = document.createElement("p");
+    anioDetalle.innerHTML = "<strong>Año:</strong> " + data.Year;
+    
+    let botonQuitar = document.createElement("button");
+    botonQuitar.innerHTML = "Volver";
+
+    divMaquetado.appendChild(tituloDetalle);
+    divMaquetado.appendChild(posterDetalle);
+    divMaquetado.appendChild(directorDetalle);
+    divMaquetado.appendChild(actoresDetalle);
+    divMaquetado.appendChild(sinopsisDetalle);
+    divMaquetado.appendChild(anioDetalle);
+    divMaquetado.appendChild(botonQuitar);
+
+    divFlotanteDetalle.appendChild(divMaquetado);
+    document.body.appendChild(divFlotanteDetalle);
+
+    //evento para salir del detalle
+    botonQuitar.addEventListener("click", ()=>{
+        divFlotanteDetalle.remove(); //quita el detalle
+        contenedor.classList.remove("opaco");
+        document.body.classList.remove("estatico");
+        detenerScroll = false;
+    })
+}
+
+function cambiarPage(){
+    let boton = document.getElementById("empezar");
+    let landin = document.getElementById("landing-page");
+    let gen = document.getElementById("general");
+    let btn2 = document.getElementById("btnBuscar");
+    boton.addEventListener("click", (e)=>{
+        if(e.target){
+            landin.style.visibility = "hidden";
+            gen.style.visibility = "visible";
+            btn2.style.visibility = "visible";
+        }
+    })
+}
+
+function construirUrl(){
+    let url = "http://www.omdbapi.com/?s="+nombre+"&apikey=c0f0795e&page="+cont;
+
+    if (anio) {
+        url += "&y=" + anio;
+    }
+    return url;
+}
+
+function lanzaPeticion(){
+    const url = construirUrl();
+
+    fetch(url).then(response => response.json())
+    .then(data => {
+        if (cont === 1) {
+            contenedor.innerHTML = '';
+        }
+        maquetar(data);
+        cargando = false;
+    })
+}
+
+function getName(){
+    return input ? input.value : "";
+}
+
+function getNameAno(){
+    return inputAnio ? inputAnio.value : "";
+}
+
+
+function buscar() {
+    let nuevoNombre = getName();
+    let nuevoAnio = getNameAno();
+
+    if (nuevoNombre) {
+        nombre = nuevoNombre;
+        anio = nuevoAnio;
+        cont = 1; 
+        contenedor.innerHTML = '';
+        lanzaPeticion();
+    }
+}
+
+function keydown(e) {
+    if(e.key === "Enter") {
+        buscar();
+    }
+}
+
+function funcionar(){
+    document.getElementById("addName").addEventListener("click", buscar); 
+    if(input) input.addEventListener("keydown", keydown);
+    if(inputAnio) inputAnio.addEventListener("keydown", keydown);
+}
+
+
+
+window.onload = ()=>{
+    contenedor = document.getElementById("general");
+    input = document.getElementById("input");
+    inputAnio = document.getElementById("inputAnio"); 
+    cambiarPage();
+    funcionar();
+}
+
+if(!detenerScroll){
+    window.onscroll = () =>{
+    console.log(window.scrollY);
+    let cercaAbajo = (window.innerHeight + window.scrollY >= document.body.offsetHeight -350);
+
+    if(cercaAbajo && !cargando){
+        cargando = true;
+        cont++; 
+        lanzaPeticion();
+    }
+}
 }
